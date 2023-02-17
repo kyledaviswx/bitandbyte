4857c34ba8526dc6039ae0e569931c056e9abce5
Kyle Davis <kdavis@instructors.2u.com>
2023-02-16 21:29:23 -0600

    basically finished now with main site and content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
 import Home from "./pages/Home";
+import bookCover from "./assets/frontcover_partial.jpg";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Bit and Byte - Adventures in coding</h1>
-        <h2>Hunt for the Sunken Treasure</h2>
-        <h3>The adventure is coming soon!</h3>
-      </header>
-      <body>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossorigin="anonymous"
+      />
+      <Container>
+        <Row>
+          <Col>
+            <header style={{ paddingLeft: 0 }}>
+              <Image className="img-fluid" src={bookCover}></Image>
+            </header>
+          </Col>
+        </Row>
+      </Container>
+      <body style={{ marginTop: "20px" }}>
         <Home />
       </body>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,75 @@
 import React from "react";
-import coverImage from "../assets/frontcover.jpg";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import treasure from "../assets/treasure.png";
+import kyle from "../assets/Kyle.png";
+import buyNowButton from "../assets/buy-on-amazon-button.png";
 
 const Home = () => {
   return (
-    <div className="container">
-      <img className="coverImg" src={coverImage}></img>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Order Bit & Byte on Amazon and begin your own adventure!</h1>
+        </Col>
+        <Col>
+          {/* <div className="amzLinkDiv">
+            <a href="https://google.com" target="_blank" rel="noreferrer">
+              <img src={buyNowButton} alt="example" />
+            </a>
+          </div> */}
+          <h2>Available on Amazon soon!</h2>
+          <div className="amzLinkDiv">
+            <img style={{ opacity: "0.4" }} src={buyNowButton} alt="example" />
+          </div>
+        </Col>
+      </Row>
+      <hr class="fancy-line"></hr>
+      <Row>
+        <Col md={12} lg={6}>
+          <img alt="" style={{ width: "330px" }} src={treasure}></img>
+        </Col>
+        <Col md={12} lg={6}>
+          <h2>About the book</h2>
+          <p>
+            Computer programming is truly for everyone! Start learning early
+            with Bit and Byte's Adventures in Coding. Kids (and adults!) of any
+            age will be taken on an underwater adventure to find sunken
+            treasure. Using math and programming basics, Bit and Byte solve
+            problems to get the loot! Bit is a fearless and incredibly smart
+            older sister to Byte. Byte is a boy that is just beginning to learn
+            programming, just like the readers. No matter what level of learning
+            you are at, this entertaining story will keep you captivated.
+            Learning through adventure will put you right in the action along
+            with Bit and Byte. This is just the beginning! Where will
+            programming take YOU, Bit, and Byte next time?
+          </p>
+        </Col>
+      </Row>
+      <hr class="fancy-line"></hr>
+      <Row>
+        <Col md={12} lg={6}>
+          <h2>About the Author</h2>
+          <p>
+            Kyle Davis is a full-stack developer from Oklahoma. He is a proud
+            Native American that descends from Cherokee, Choctaw and Delaware
+            tribes. Growing up with a love for weather, he began his career in
+            Meteorology with a degree from the University of Oklahoma. In 2009,
+            he wanted to make a phone app, so he bought a programming book and
+            began his own new adventure. He immediately fell in love with
+            programming and the challenges that come with it. Kyle and his wife
+            Aimee have two wonderful children, Spencer and Clara. While reading
+            to them one night, he wished there was a way to teach them
+            programming with a fun story. Kids don't want to use a boring
+            textbook! The idea for Bit and Byte was born!
+          </p>
+        </Col>
+        <Col md={12} lg={6}>
+          <img alt="" style={{ width: "330px" }} src={kyle}></img>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
